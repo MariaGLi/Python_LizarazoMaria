bbf0833b8afce370f233e6892235248fa14131e0
MariaGLi <lizarazoleal27@gmail.com>
2024-04-17 10:55:46 -0500

    2.0

--- Dia6/EliminaLosRepetidos.py
+++ Dia6/EliminaLosRepetidos.py
@@ -1,5 +1,5 @@
 numeros=[]
-<<<<<<< HEAD
+
 numeros1=int(input("digita la cantidad de numeros de la lista, no superior a 300 \n"))
 assert 0< numeros1 <300 ,"¡OJO! compañero el numero de la lista no es el correcto."
 
@@ -10,13 +10,3 @@ num= list(set(numeros))
 num.sort()
 
 print(num)
-=======
-numeros1=int(input("digita la cantidad de numeros \n"))
-
-for i in range (0, numeros1):
-    numeros.append(input())
-
-unicos=list(set(numeros))
-print(unicos)
-
->>>>>>> ead614e7079ca569ea97a450ae41b558d533ca4c
